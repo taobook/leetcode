2b395232518ea4ce8282ad884910f745ed4d1dbb
George Tao <yultao@gmail.com>
2024-10-08 20:56:46 -0400

    plugin

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 > 
 > **without gathering small streams, one cannot form a river or sea.**
 > 
-> —— Xunzi (c. 310 – c. 235 BC, alt. c. 314 – c. 217 BC)# leetcode
+> —— Xunzi (c. 310 – c. 235 BC, alt. c. 314 – c. 217 BC)
---
+++ docsify-plugin-footer.js
@@ -0,0 +1,55 @@
+(function () {
+  var myPlugin = function (hook, vm) {
+
+
+    hook.init(function () {
+      // ...
+    });
+
+    // Invoked one time when the docsify instance has mounted on the DOM
+    hook.mounted(function () {
+      // ...
+    });
+
+    // Invoked on each page load before new markdown is transformed to HTML.
+    // Supports asynchronous tasks (see beforeEach documentation for details).
+    hook.beforeEach(function (markdown) {
+      // ...
+      return markdown;
+    });
+
+    // Invoked on each page load after new markdown has been transformed to HTML.
+    // Supports asynchronous tasks (see afterEach documentation for details).
+    hook.afterEach(function (html) {
+      // ...
+      var url =
+                  'https://github.com/taobook/leetcode/blob/main' +
+                  vm.route.path
+                  +".md";
+
+      // Invoked one time when docsify script is initialized
+      var footer = [
+        '<hr/>',
+        '<footer>',
+        '<span>2024 &copy; taobook. </span>',
+        '<a target="_blank" href="'+url+'">[Edit]</a>',
+        '</footer>',
+      ].join('');
+      return html + footer;
+    });
+
+    // Invoked on each page load after new HTML has been appended to the DOM
+    hook.doneEach(function () {
+      // ...
+    });
+
+    // Invoked one time after rendering the initial page
+    hook.ready(function () {
+      // ...
+    });
+  };
+
+  // Add plugin to docsify's plugin array
+  $docsify = $docsify || {};
+  $docsify.plugins = [].concat(myPlugin, $docsify.plugins || []);
+})();
